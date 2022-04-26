7a2b8c864adb15bca87ea12ddfea661ae15480ba
Alexandre <alesaudate@gmail.com>
2022-04-26 22:59:54 +0200

    master upgrade JUnit version

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,8 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
 tasks.getByName<Test>("test") {
@@ -41,7 +41,7 @@ tasks.jacocoTestCoverageVerification {
     violationRules {
         rule {
             limit {
-                minimum = "1.0".toBigDecimal()
+                minimum = "1.0".toBigDecimal()  //100% coverage
             }
         }
     }
